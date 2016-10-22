b5105410c3be1ef57767b80766ff0b958d3796d2
Nelorth <nikolas.kirschstein@gmail.com>
2016-10-22 15:33:28 +0200

    Gameloop und wichtige Methoden implementiert :)

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,25 @@
-/**
- * Created by Nelorth on 22.10.2016.
- */
 public class Game {
+    private boolean running;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void start() {
+        while (running) {
+
+        }
+    }
 }
