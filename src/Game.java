9e4a63c08602f1e7dd45e605e7d2bf401cdf23b2
Nelorth <nikolas.kirschstein@gmail.com>
2016-10-23 13:45:30 +0200

    Hauptmethoden dokumentiert ^^

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,61 @@
+/**
+ * @author Kirschi
+ */
+
 public class Game {
     private boolean running;
 
+    /**
+     * Die öffentliche statische Leere namens "main"
+     * @param args Kommondozeilenargumente
+     */
     public static void main(String[] args) {
-
+        new Game().start();
     }
 
-    public void init() {
-
-    }
-
-    public void render() {
-
-    }
-
-    public void update() {
-
-    }
-
-    public void start() {
+    /**
+     * Diese Methode startet das Spiel :O
+     */
+    private void start() {
+        init();
         while (running) {
-
+            update();
+            render();
         }
+    }
+
+    /**
+     * Diese Methode stoppt das Spiel!!
+     */
+    public void stop() {
+        running = false;
+    }
+
+    /**
+     * Initialisierungsgedöns
+     */
+    private void init() {
+        running = true;
+    }
+
+    /**
+     * Spieleupdates
+     *  - Benutzerinput verarbeiten
+     *  - Veränderungen im Spiel berechnen
+     *  - Postprocessing (v.a. Physik)
+     */
+    private void update() {
+        //...
+    }
+
+    /**
+     * Rendern des aktuellen Spielstatus
+     *  - Nicht sichtbare Objekte erfassen
+     *  - Sichtbare Objekte verarbeiten
+     *  - Auf Hintergrundpuffer zeichnen
+     *  - Puffer als Gesamtheit anzeigen
+     */
+    private void render() {
+        //...
     }
 }
