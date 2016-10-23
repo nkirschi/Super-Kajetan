05149a680dadd8b4e15c3ec91b986b54325a3e5e
Nelorth <nikolas.kirschstein@gmail.com>
2016-10-23 18:09:29 +0200

    Testbuttons hinzugefügt :O

--- src/gui/MenuView.java
+++ src/gui/MenuView.java
@@ -1,10 +1,22 @@
 package gui;
 
+import game.Level;
+
 import javax.swing.*;
 
 /**
  * @author Kirschi
  */
 public class MenuView extends AbstractView {
+    private JButton startButton;
+    private JButton pauseButton;
 
+    public MenuView() {
+        startButton = new JButton("Start");
+        pauseButton = new JButton("Pause");
+        startButton.addActionListener(l -> Level.getInstance().start());
+        pauseButton.addActionListener(l -> Level.getInstance().pause());
+        add(startButton);
+        add(pauseButton);
+    }
 }
