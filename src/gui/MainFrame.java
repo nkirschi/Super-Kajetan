13146cca48b7f0423e8d65aab00b7809c2195b21
Nelorth <nikolas.kirschstein@gmail.com>
2017-05-08 17:55:34 +0200

    Alles. Einfach ALLES!!!

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -5,6 +5,9 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * Klasse des Hauptfensters
+ */
 public class MainFrame extends JFrame {
 
     private final int WIDTH = 1024, HEIGHT = 768;
@@ -16,19 +19,18 @@ public class MainFrame extends JFrame {
      */
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
-        } catch ( Exception e ) {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
             e.printStackTrace();
         }
         new MainFrame();
     }
 
     public MainFrame() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(d.width / 2 - WIDTH / 2, d.height / 2 - HEIGHT / 2);
-
         currentView = new MenuView(this);
         add(currentView);
         setVisible(true);
