075af41073c7d65ad3ab25f0d5799861d0bd0555
Nelorth <nikolas.kirschstein@gmail.com>
2017-05-09 16:56:33 +0200

    Programmbeendigungsbestätigung hinzugefügt :P

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -2,13 +2,13 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 /**
  * Klasse des Hauptfensters
  */
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements WindowListener {
 
     private final int WIDTH = 1024, HEIGHT = 768;
     private AbstractView currentView;
@@ -31,8 +31,35 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(d.width / 2 - WIDTH / 2, d.height / 2 - HEIGHT / 2);
+        addWindowListener(this);
         currentView = new MenuView(this);
         add(currentView);
         setVisible(true);
+    }
+
+    public void windowOpened(WindowEvent windowEvent) {
+    }
+
+    public void windowClosing(WindowEvent e) {
+        int result = JOptionPane.showConfirmDialog(this, "Wirklich beenden?", "", 0);
+        if(result == 0) {
+            System.exit(0);
+        }
+
+    }
+
+    public void windowClosed(WindowEvent windowEvent) {
+    }
+
+    public void windowIconified(WindowEvent windowEvent) {
+    }
+
+    public void windowDeiconified(WindowEvent windowEvent) {
+    }
+
+    public void windowActivated(WindowEvent windowEvent) {
+    }
+
+    public void windowDeactivated(WindowEvent windowEvent) {
     }
 }
