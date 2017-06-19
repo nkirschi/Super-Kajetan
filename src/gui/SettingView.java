22088d22d18a4f53e09a2fb5bfe831f6944f1635
lumpenputzer <lumpenputzer@users.noreply.github.com>
2017-06-19 12:46:09 +0200

    Update SettingView.java

--- src/gui/SettingView.java
+++ src/gui/SettingView.java
@@ -1,6 +1,18 @@
 class SettingView extends AbstractView{
   private MainFrame mainframe;
+  private static SettingView instance;
   
-  private void update(){
+  private SettingView (MainFrame mainframe){
+    super(mainframe);
   }
+  
+  public static SettingView instance(MainFrame mainframe){
+    if(instance==null){
+      instance = new SettingView(mainframe);
+    }
+    return instance;
+  }
+  
+  public void update(){
+}
 }
