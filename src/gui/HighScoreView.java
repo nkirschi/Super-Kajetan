f0351a77105400a5963b3d7ebf8034d75c6d95d7
lumpenputzer <lumpenputzer@users.noreply.github.com>
2017-06-19 12:41:49 +0200

    Update HighScoreView.java

--- src/gui/HighScoreView.java
+++ src/gui/HighScoreView.java
@@ -1,6 +1,18 @@
 class HighScoreView extends AbstractView{
   private MainFrame mainframe;
+  private static HighScoreView instance;
   
-  private void update(){
+  private HighScoreView (MainFrame mainframe){
+    super(mainframe);
+  }
+  
+  public static HighScoreView instance(MainFrame mainframe){
+    if(instance==null){
+      instance = new HighScoreView(mainframe);
+    }
+    return instance;
+  }
+  
+  public void update(){
   }
 }
