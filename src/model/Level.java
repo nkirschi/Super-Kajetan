0d1746c9c4d61320ed91a71a04f02b24b7991942
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-19 12:54:15 +0200

    Update Level.java

--- src/model/Level.java
+++ src/model/Level.java
@@ -1,3 +1,13 @@
-public abstract class Level {
+import util.list.List;
+
+public class Level {
+    private String backgroundFilePath;
+    private List<Obstacle> obstacles;
+    private List<Entity> entities;
     
+    public Level(String backgroundFilePath, List<Obstacle> obstacles, List<Entity> entities) {
+        this.backgroundFilePath = backgroundFilePath;
+        this.obstacles = obstacles;
+        this.entities = entities;
+    }
 }
