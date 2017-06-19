377d1098eb5a05486a0c04801e185ae551a10b83
ImDexter <doenermann2011@gmx.de>
2017-06-19 12:54:19 +0200

    Update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,7 +1,20 @@
 import util.point
 
 public class Player extends Entity {
+
     public point position;
-  public class Player (Point position) {
-    
+  public Player (Point position) {
+    /** Player spawnt auf Position (0,0)
+    */
+    position = (0,0);
   }
+    /** gibt Position des Spielers aus
+    */
+    public Point givePosition(){
+        return postition;
+    }
+    
+    public void move() {
+        
+    }
+}
