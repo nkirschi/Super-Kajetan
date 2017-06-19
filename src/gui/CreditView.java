88d5226ef3d51b77287ce76e9c47667c5d23cfef
lumpenputzer <lumpenputzer@users.noreply.github.com>
2017-06-19 12:40:16 +0200

    Update CreditView.java

--- src/gui/CreditView.java
+++ src/gui/CreditView.java
@@ -1,6 +1,18 @@
 class CreditView extends AbstractView{
   private MainFrame mainframe;
+  private static CreditView instance;
   
-  private void update(){
+  private CreditView (MainFrame mainframe){
+    super(mainframe);
+  }
+  
+  public static CreditView instance(MainFrame mainframe){
+    if(instance==null){
+      instance = new CreditView(mainframe);
+    }
+    return instance;
+  }
+  
+  public void update(){
   }
 }
