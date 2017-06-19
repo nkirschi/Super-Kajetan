bcdafa15cd870d0ef01e68d2f83795ebfe593f80
lumpenputzer <lumpenputzer@users.noreply.github.com>
2017-06-19 12:42:47 +0200

    Update LevelLobbyView.java

--- src/gui/LevelLobbyView.java
+++ src/gui/LevelLobbyView.java
@@ -1,6 +1,18 @@
 class LevelLobbyView extends AbstractView{
   private MainFrame mainframe;
+  private static LevelLobbyView instance;
   
-  private void update(){
+  private LevelLobbyView (MainFrame mainframe){
+    super(mainframe);
+  }
+  
+  public static LevelLobbyView instance(MainFrame mainframe){
+    if(instance==null){
+      instance = new LevelLobbyView(mainframe);
+    }
+    return instance;
+  }
+  
+  public void update(){
   }
 }
