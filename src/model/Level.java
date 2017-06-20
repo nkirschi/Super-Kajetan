0aef173fa373a4ccce626aecec96db30f9846170
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-20 08:30:52 +0200

    Update Level.java

--- src/model/Level.java
+++ src/model/Level.java
@@ -6,10 +6,27 @@ public class Level {
     private String backgroundFilePath;
     private List<Obstacle> obstacles;
     private List<Entity> entities;
+    private double length; // Länge des Levels in px
 
     public Level(String backgroundFilePath, List<Obstacle> obstacles, List<Entity> entities) {
         this.backgroundFilePath = backgroundFilePath;
         this.obstacles = obstacles;
         this.entities = entities;
+    }
+    
+    public String getBackgroundFilePath() {
+        return backgroundFilePath;
+    }
+    
+    public List<Obstacle> getObstacles() {
+        return obstacles;
+    }
+    
+    public List<Entity> getEntities() {
+        return entities;
+    }
+    
+    public double getLength() {
+        return length;
     }
 }
