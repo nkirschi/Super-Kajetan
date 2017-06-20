89496bcf3444ef35b2d1efbe8a102ad26227b409
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-20 07:57:39 +0200

    Create GameConstants.java

---
+++ src/physics/GameConstants.java
@@ -0,0 +1,5 @@
+public class GameConstants {
+    public static final double GAME_HERTZ = 30.0;
+    final double TIME_BETWEEN_UPDATES = 1000000000 / GAME_HERTZ;
+    final int MAX_UPDATES_BEFORE_RENDER = 5;
+}
