3375b563ebdbcdf31c6ee620b47e2ad6d9045538
ImDexter <doenermann2011@gmx.de>
2017-06-20 08:40:52 +0200

    Update Spider.java

--- src/model/Spider.java
+++ src/model/Spider.java
@@ -1,1 +1,21 @@
+package model;
 
+import util.Point;
+
+public class Spider extends Enemy {
+  
+    public Spider (Point position) {
+      position = new Point();
+      
+    }
+      
+     public Point getPosition(){
+        return position; 
+      }
+      
+      public void move (double x, double y) {
+          position.move(x, y); 
+      }
+   
+  
+}
