61716b9cb2bcf79d661da452e886ff9001512098
ImDexter <doenermann2011@gmx.de>
2017-06-20 08:28:59 +0200

    Update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -8,15 +8,27 @@ public class Player extends Entity {
         position = new Point();
     }
 
-    /**
-     * Getter-Methode für die aktuelle Spielerposition
-     * @return Position des Spielers als Point
-     */
+        /**
+        * Getter-Methode für die aktuelle Spielerposition
+        * @return Position des Spielers als Point
+        */
     public Point getPosition() {
         return position;
     }
-
+    
+        /**
+        * bewegt den Spieler
+        */
+    
     public void move(double x, double y) {
         position.move(x, y);
+    }
+    
+        /**
+        * läd Bild aus images/...
+        */
+    
+    public void loadImage () {
+        ImageUtil.getImage(images/);
     }
 }
