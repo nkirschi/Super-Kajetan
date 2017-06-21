f0bea50b914190f36b5087ecea242060e4f17189
ImDexter <doenermann2011@gmx.de>
2017-06-21 10:11:06 +0200

    Update Knight.java

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -1,5 +1,26 @@
 package model;
+
 import util.Point;
-public class Spider extends Enemy {    public Spider(Point position) {        position = new Point();    }
-    public Point getPosition() {        return position;    }
-    public void move(double x, double y) {        position.move(x, y);    }        public void loadImage() {        try {            ImageUtil.getImage("images");           }   catch (IOExecption e) {            e.ptrintStackTrace();           }    }}
+
+public class Knight extends Enemy {
+    public Knight (Point position){
+     position = new Point();   
+    }
+    
+    public Point getPosition(){
+        return position;   
+    }
+    
+    public void move (doublex, double y){
+     position.move(x,y);   
+    }
+    
+    public void loadImage(){
+        try {
+            ImageUtil.getImage("images");
+        }
+            catch (IOExecption e){
+             e,printStackTrace();
+        }
+    }
+}
