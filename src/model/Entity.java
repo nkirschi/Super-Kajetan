2b292adfeb867da6b655c96b3dc486173b659a26
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-21 09:47:47 +0200

    Update Entity.java

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -8,6 +8,7 @@ import java.awt.*;
 public abstract class Entity implements Collidable {
     protected Point position;
     protected Rectangle hitbox;
+    protected int health;
 
     public Rectangle getHitbox() {
         return hitbox;
