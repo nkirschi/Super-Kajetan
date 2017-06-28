0b15c99ac20abda5765ea6dfebb3dcc7bc596745
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-28 14:15:01 +0200

    Zurück-Button Inception!

--- src/gui/CreditsView.java
+++ src/gui/CreditsView.java
@@ -1,10 +1,26 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 class CreditsView extends AbstractView {
     private static CreditsView instance;
 
     private CreditsView() {
         super();
+        setLayout(new BorderLayout());
+        setBackground(GUIConstants.MENU_BACKGROUND_COLOR);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.setBackground(GUIConstants.MENU_BACKGROUND_COLOR);
+
+        JButton backButton = new JButton("Zurück");
+        backButton.setBackground(GUIConstants.BUTTON_COLOR);
+        //backButton.setPreferredSize(GUIConstants.defaultButtonSize);
+        backButton.addActionListener(a -> MainFrame.getInstance().changeTo(MainMenuView.getInstance()));
+
+        buttonPanel.add(backButton);
+        add(buttonPanel, BorderLayout.PAGE_END);
     }
 
     public void update() {
