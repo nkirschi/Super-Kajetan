1b14f5ce9d22f547fc2d6a989caed4e27b2b17aa
lumpenputzer <benedikt@moedl.org>
2017-06-28 19:44:20 +0200

    *Fontänen

--- src/gui/HighscoresView.java
+++ src/gui/HighscoresView.java
@@ -35,11 +35,11 @@ class HighscoresView extends AbstractView {
 
         buttonPanel.add(backButton);
         add(buttonPanel, BorderLayout.PAGE_END);
-
-        update(); //Initialisiert die eigentliche Liste, damit diese auch immer aktuell ist
     }
 
     private JPanel initHighScoreList(){
+        Font defaultFont = Constants.DEFAULT_FONT.deriveFont(18F);
+
         JPanel list = new JPanel(new FlowLayout());
         list.setBackground(Constants.MENU_BACKGROUND_COLOR);
 
@@ -52,20 +52,19 @@ class HighscoresView extends AbstractView {
 
         try {
             ImageIcon trophyImage = ImageUtil.getIcon("images/trophy.png");
-            System.out.println(trophyImage.getIconHeight()+"   "+trophyImage.getIconWidth());
             JLabel trophyLabel = new JLabel(trophyImage);
             trophyLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
             fancyCollumPanel.add(trophyLabel);
 
-            //Setzt jetzt die Border der Spalten-Überschriften entsprechend, um für Höhe des Bildes zu kompensieren. (15/2 kompensiert für texthöhe. unschön, wissen wir ....)
-            //TODO textgröße irgendwie anders bestimmen!!!!!
-            listCollumHeaderBorder = BorderFactory.createEmptyBorder(trophyImage.getIconHeight()/2-15/2,0,trophyImage.getIconHeight()/2-15/2,0);
+            //Setzt jetzt die Border der Spalten-Überschriften entsprechend, um für Höhe des Bildes zu kompensieren.
+            listCollumHeaderBorder = BorderFactory.createEmptyBorder(trophyImage.getIconHeight()/2-defaultFont.getSize()/2,0,trophyImage.getIconHeight()/2-defaultFont.getSize()/2,0);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         for(int i = 1;i<11;i++ ){
             JLabel label = new JLabel("Platz "+i);
+            label.setFont(defaultFont);
             label.setAlignmentX(Component.CENTER_ALIGNMENT);
             label.setBorder(listCellBorder);
             fancyCollumPanel.add(label);
@@ -90,14 +89,17 @@ class HighscoresView extends AbstractView {
 
         //Überschriften der Spalten
         JLabel collumName = new JLabel(Constants.DB_COLLUM_NAME);
+        collumName.setFont(defaultFont);
         collumName.setAlignmentX(Component.CENTER_ALIGNMENT);
         collumName.setBorder(listCollumHeaderBorder);
         namePanel.add(collumName);
         JLabel collumScore = new JLabel(Constants.DB_COLLUM_SCORE);
+        collumScore.setFont(defaultFont);
         collumScore.setAlignmentX(Component.CENTER_ALIGNMENT);
         collumScore.setBorder(listCollumHeaderBorder);
         scorePanel.add(collumScore);
         JLabel collumDate = new JLabel(Constants.DB_COLLUM_DATE);
+        collumDate.setFont(defaultFont);
         collumDate.setAlignmentX(Component.CENTER_ALIGNMENT);
         collumDate.setBorder(listCollumHeaderBorder);
         datePanel.add(collumDate);
@@ -107,11 +109,13 @@ class HighscoresView extends AbstractView {
             ResultSet highScoreSet = DBConnection.getInstance().query("SELECT * FROM " + Constants.DB_TABLE + " ORDER BY " + Constants.DB_COLLUM_SCORE + " DESC LIMIT 10;");
             while(highScoreSet.next()){
                 JLabel nameCell = new JLabel(highScoreSet.getString(Constants.DB_COLLUM_NAME));
+                nameCell.setFont(defaultFont);
                 nameCell.setAlignmentX(Component.CENTER_ALIGNMENT);
                 nameCell.setBorder(listCellBorder);
                 namePanel.add(nameCell);
 
                 JLabel scoreCell = new JLabel(Integer.toString(highScoreSet.getInt(Constants.DB_COLLUM_SCORE)));
+                scoreCell.setFont(defaultFont);
                 scoreCell.setAlignmentX(Component.CENTER_ALIGNMENT);
                 scoreCell.setBorder(listCellBorder);
                 scorePanel.add(scoreCell);
@@ -121,6 +125,7 @@ class HighscoresView extends AbstractView {
                 String dateString = dateFormat.format(date);
 
                 JLabel dateCell = new JLabel(dateString);
+                dateCell.setFont(defaultFont);
                 dateCell.setAlignmentX(Component.CENTER_ALIGNMENT);
                 dateCell.setBorder(listCellBorder);
                 datePanel.add(dateCell);
