431cfc42a2d69ec7138c9a72e47004caf9e6a2bd
Nelorth <nikolas.kirschstein@gmail.com>
2017-06-29 22:44:06 +0200

    Hinterherräumen *seufz*

--- src/model/Crate.java
+++ src/model/Crate.java
@@ -10,16 +10,11 @@ public abstract class Crate extends Obstacle {
     private final double CRATE_WIDTH = 100;
     private final double CRATE_HEIGHT = 100;
     protected Rectangle2D.Double hitbox;
-    
-    public Ground(Point position) {
+
+    public Crate(Point position) {
         this.position = position;
         hitbox = new Rectangle2D.Double(position.getX() - CRATE_WIDTH / 2, position.getY() - CRATE_HEIGHT, CRATE_WIDTH, CRATE_HEIGHT);
         
-    }
-
-    @Override
-    public Rectangle2D.Double getHitbox() {
-        return hitbox;
     }
     
     public String getImagePath() {
