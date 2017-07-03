da7e5aab1987c8d00d75da4c71460372147e338c
Nelorth <nikolas.kirschstein@gmail.com>
2017-07-03 15:33:37 +0200

    MasterGainNotSupported gecatched

--- src/util/AudioPlayer.java
+++ src/util/AudioPlayer.java
@@ -84,9 +84,14 @@ public class AudioPlayer {
                         FloatControl volumeControl = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
                         volumeControl.setValue(volume);
                     } catch (Exception e) {
-                        e.printStackTrace();
-                        Logger.log("Volume Control wird auf dem System nicht unterstützt", Logger.WARNING);
-                        Logger.log(e, Logger.WARNING);
+                        try {
+                            FloatControl volumeControl = (FloatControl) line.getControl(FloatControl.Type.VOLUME);
+                            volumeControl.setValue(volume);
+                        } catch (Exception f) {
+                            e.printStackTrace();
+                            Logger.log("Volume Control wird auf dem System nicht unterstützt", Logger.WARNING);
+                            Logger.log(e, Logger.WARNING);
+                        }
                     }
                     line.start();
                     stream(getAudioInputStream(outFormat, in), line);
