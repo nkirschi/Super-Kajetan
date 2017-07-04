95edc9a53a6d6eac21f01923bb6f00cb36786af7
ImFabi <fabian.weinelt@gmx.de>
2017-07-04 08:11:13 +0200

    Update HighscoresView.java

--- src/gui/HighscoresView.java
+++ src/gui/HighscoresView.java
@@ -155,7 +155,8 @@ class HighscoresView extends AbstractView {
             Logger.log("Fehler beim initialisieren der Highscores", Logger.WARNING);
             Logger.log(e, Logger.WARNING);
         }
-
+        
+        Logger.log("Highscores initialisiert", Logger.INFO);
         return listTopLevelPanel;
     }
 
