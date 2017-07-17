f4f576e51df6afb71ba809a2b40ede28e5f40e36
Nelorth <nikolas.kirschstein@gmail.com>
2017-07-17 16:26:03 +0200

    REPAIR (grunz)

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -5,29 +5,55 @@ import logic.Behavior;
 import java.awt.geom.Rectangle2D;
 
 public class Knight extends Enemy {
-    private final double SPIDER_WIDTH = 90;
-    private final double SPIDER_HEIGHT = 160;
+    private final double SKELETON_WIDTH = 95;
+    private final double SKELETON_HEIGHT = 230;
 
     public Knight(double x, double y, Behavior behavior, Direction viewingDirection) {
         super(x, y, behavior, viewingDirection);
-        hitbox = new Rectangle2D.Double(x - SPIDER_WIDTH / 2, y - SPIDER_HEIGHT, SPIDER_WIDTH, SPIDER_HEIGHT);
-        viewingRange = 500;
-        attackRange = 100;
-        health = getMaxHealth();
-        worthiness = 5;
-    }
-
-    public String getImagePath() {
-        return "images/enemies/skeleton_1.png";
+        hitbox = new Rectangle2D.Double(x - SKELETON_WIDTH / 2, y - SKELETON_HEIGHT, SKELETON_WIDTH, SKELETON_HEIGHT);
+        viewingRange = 1000;
+        attackRange = 200;
+        health = 500;
+        strength = 2;
+        worthiness = 10;
     }
 
     @Override
     public int getMaxHealth() {
-        return 200;
+        return 500;
+    }
+
+    @Override
+    public void setWalking(boolean walking) {
+        if (walking)
+            walkCount++;
+        super.setWalking(walking);
+    }
+
+    @Override
+    public void setRunning(boolean running) {
+        if (running)
+            walkCount++;
+        super.setRunning(running);
+    }
+
+    @Override
+    public void setCrouching(boolean crouching) {
+        double crouchingDelta = 53;
+        if (crouching && !this.crouching)
+            hitbox.setRect(hitbox.x, hitbox.y + crouchingDelta, hitbox.width, hitbox.height - crouchingDelta);
+        else if (!crouching && this.crouching)
+            hitbox.setRect(hitbox.x, hitbox.y - crouchingDelta, hitbox.width, hitbox.height + crouchingDelta);
+
+        super.setCrouching(crouching);
+    }
+
+    public String getImagePath() {
+        return "images/char/char_stand.png";
     }
 
     @Override
     public String toString() {
-        return "Skeleton at " + super.toString();
+        return "Kight at " + super.toString();
     }
 }
