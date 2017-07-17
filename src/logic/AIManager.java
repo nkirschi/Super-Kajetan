023d4ab767abfc968e71a9b52af14e3fcd8a91b3
Nelorth <nikolas.kirschstein@gmail.com>
2017-07-17 20:03:18 +0200

    Billigste Lösung :D

--- src/logic/AIManager.java
+++ src/logic/AIManager.java
@@ -11,10 +11,12 @@ import static logic.Behavior.ATTACK;
 public class AIManager {
     CollisionHandler collisionHandler;
     private int patrolCount;
+    private long lastSecond;
 
     public AIManager(CollisionHandler collisionHandler) {
         this.collisionHandler = collisionHandler;
         patrolCount = 0;
+        lastSecond = System.nanoTime();
     }
 
     public void handleAI(Level level, Player player) {
@@ -143,7 +145,10 @@ public class AIManager {
     }
 
     private void attack(Enemy enemy, Player player) {
-        player.suffer(enemy.getStrength());
+        if (System.nanoTime() - lastSecond > 1000000000) {
+            player.suffer(enemy.getStrength());
+            lastSecond = System.nanoTime();
+        }
     }
 
     private Obstacle nearestViewblocker(Enemy enemy, Level level) {
