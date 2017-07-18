20905b6704a1ee2820d41620144281b6580ecbb2
ImFabi <fabian.weinelt@gmx.de>
2017-07-18 08:28:56 +0200

    Image Path noch zu beabeiten

---
+++ src/model/Coin.java
@@ -0,0 +1,42 @@
+package model;
+
+import logic.Behavior;
+
+import java.awt.geom.Rectangle2D;
+
+public class Coin extends Enemy {
+    private final double COIN_WIDTH = 90;
+    private final double COIN_HEIGHT = 90;
+
+    public Skeleton(double x, double y, Behavior behavior, Direction viewingDirection) {
+        super(x, y, behavior, viewingDirection);
+        hitbox = new Rectangle2D.Double(x - COIN_WIDTH / 2, y - COIN_HEIGHT, COIN_WIDTH, COIN_HEIGHT);
+        viewingRange = 500;
+        attackRange = 100;
+        health = getMaxHealth();
+        worthiness = 10;
+        strength = 0;
+    }
+
+    public String getImagePath() {
+        if (walking) {
+            if(walkcount <25) {
+                return "images/enemies/enemy_skeleton_walk_1.png";
+            }
+            else {
+                return "images/enemies/enemy_skeleton_walk_2.png";   
+            }
+        }
+        
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin at " + super.toString();
+    }
+}
